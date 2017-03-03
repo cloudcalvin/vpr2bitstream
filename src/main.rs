8f1c8d217133a896e2cb4c5c57fd32cbc2bc405a
cloudcalvin <cloudcalvin@me.com>
2017-03-03 20:57:14 +0200

    initialize project

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+  println!("Hello, world!");
+
+
+  //read the .place file, and generate the placement bitstream based on input parameters and mapping file.
+  //read the .route file and generate the routing bitstream based on input parameters and mapping file.
+
+/*
+
+  settings (coordinate system (vpr-style == cartesian))
+  read file ( .place )
+    place file consists of:
+      file name 
+      array size 
+      space
+      header
+      placement-data 
+
+  read file ( .route )
+
+    route file consists of:
+      array size 
+      \n
+      'Routing'
+      \n      
+      routing-data 
+    */
+
+    
+}
